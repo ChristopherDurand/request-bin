d56faf378987a03ae23e06e4160ced5eb3f7a2f4
ChristopherDurand <chris@cdurand.net>
2021-05-21 08:18:52 -0500

    initial commit

---
+++ front-end/src/services/bins.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const get = (slug) => {
+  const request = axios.get(`/data/${slug}`)
+  return request.then(response => {
+    return response.data
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+const create = () => {
+  const request = axios.post(`/tubs`)
+  return request.then(response => response.data.uri)
+}
+
+const bins = {
+  get,
+  create
+}
+
+export default bins
